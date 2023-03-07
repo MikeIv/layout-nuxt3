f3b1985449c13b130344147b78c5c3651681b05d
MIUIvanov <MIUIvanov@synergy.ru>
2023-03-07 22:40:05 +0300

    настройка импорта svg

--- components/Tools/index.vue
+++ components/Tools/index.vue
@@ -1,25 +1,35 @@
 <template>
-  <section class="tools layout__wrapper">
-    <h1 class="tools__title a-font__h1">Инструменты</h1>
-    <div class="tools__header">
-      <ul class="tools__header-list">
-        <li class="tools__header-item cell-01">Технологии</li>
-        <li class="tools__header-item" :class="item.cell" v-for="(item, index) in techList" :key="index">
-          <div class="tools__header-img-wrapper">
-            <!--            <IconsVue class="tools__header-image" />-->
-
-            <component :is="item.icon"></component>
-            <component :is="nameComponent"></component>
+  <section className="tools layout__wrapper">
+    <h1 className="tools__title a-font__h1">Инструменты</h1>
+    <div className="tools__header">
+      <ul className="tools__header-list">
+        <li className="tools__header-item cell-01">Технологии</li>
+        <li className="tools__header-item cell-02">
+          <div className="tools__header-img-wrapper">
+            <IconsVue class="tools__header-image" />
           </div>
-          <div class="tools__header-link-group">
-            <a :href="`${item.href}`" class="tools__header-sourse" target="_blank">{{ item.title }}</a>
-            <button class="tools__header-link">#</button>
+          <div className="tools__header-link-group">
+            <a href="https://v3.ru.vuejs.org/" className="tools__header-sourse" target="_blank">Vue.js Doc</a>
+            <button className="tools__header-link">#</button>
           </div>
         </li>
+        <li className="tools__header-item cell-03">
+          <div className="tools__header-img-wrapper">
+            <IconsNuxt class="tools__header-image" />
+          </div>
+          <div className="tools__header-link-group">
+            <a href="https://nuxt.com/" className="tools__header-sourse" target="_blank">Nuxt Doc</a>
+            <button className="tools__header-link">#</button>
+          </div>
+        </li>
+        <li className="tools__header-item cell-04">CSS</li>
+        <li className="tools__header-item cell-05">CSS</li>
+        <li className="tools__header-item cell-06">CSS</li>
+        <li className="tools__header-item cell-07">CSS</li>
       </ul>
     </div>
-    <div class="tools__section">
-      <h2 class="tools__subtitle a-font__h3">SVG</h2>
+    <div className="tools__section">
+      <h2 className="tools__subtitle a-font__h3">SVG</h2>
       <IconsTelegram />
     </div>
   </section>
@@ -28,16 +38,6 @@
 <script>
 export default {
   name: 'Tools',
-
-  data: () => ({
-    nameComponent: 'IconsVue',
-    techList: [
-      { cell: 'cell-02', icon: 'IconsVue', href: 'https://v3.ru.vuejs.org/', title: 'Vue.js Doc' },
-      { cell: 'cell-03', icon: 'IconsNuxt', href: 'https://nuxt.com/', title: 'Nuxt Doc' },
-      { cell: 'cell-04', icon: 'IconsNuxt', href: 'https://nuxt.com/', title: 'Nuxt Doc' },
-      { cell: 'cell-05', icon: 'IconsNuxt', href: 'https://nuxt.com/', title: 'Nuxt Doc' },
-    ],
-  }),
 };
 </script>
 
