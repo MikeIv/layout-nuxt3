7f87f1988d5de8bf283e42b8b5c8627c013d95af
MIUIvanov <MIUIvanov@synergy.ru>
2023-03-16 14:32:28 +0300

    Мобильное меню шаблон

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <ul class="header">
-    <li class="header__item a-font__s" v-for="(item, index) in routeLinks" :key="index">
-      <NuxtLink :to="item.route">{{ item.name }}</NuxtLink>
-    </li>
+  <section class="header">
+    <div v-if="isMobile" class="header__mobile-toggle">
+      <button class="header__toggle-nav" :class="{ active: showNav }" @click="toggleNav" ref="navigation">
+        <span></span>
+      </button>
 
-    <p v-if="isMobile">Я Мобилко</p>
-  </ul>
+      <div v-if="showNav" class="header__mobile-menu"></div>
+    </div>
+
+    <ul v-if="isDesktop || isTabled" class="header__nav">
+      <li class="header__item a-font__s" v-for="(item, index) in routeLinks" :key="index">
+        <NuxtLink :to="item.route">{{ item.name }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup>
@@ -29,6 +37,12 @@ const routeLinks = [
 ];
 
 const { isMobile, isTabled, isDesktop } = useResponsive();
+
+const showNav = ref(false);
+
+const toggleNav = () => {
+  showNav.value = !showNav.value;
+};
 </script>
 
 <style scoped lang="scss">
