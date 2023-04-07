f97e9ba3bb8cc550b04e613103cd0bb42eb259ff
MIUIvanov <MIUIvanov@synergy.ru>
2023-04-07 14:13:56 +0300

    подключение тестовых данных для страницы Портфолио

--- components/Portfolio/index.vue
+++ components/Portfolio/index.vue
@@ -1,10 +1,15 @@
 <template>
   <section class="portfolio layout__wrapper">
     <h1 class="portfolio__title a-font__h1">Портфолио</h1>
+    <li v-for="(item, index) in dataList" :key="index">{{ item.name }} - {{ index }}</li>
   </section>
 </template>
 
-<script setup></script>
+<script setup>
+const { portfolio } = portfolioData();
+console.log('DATA-PORTFOL', portfolio.value);
+const dataList = portfolio.value;
+</script>
 
 <style scoped lang="scss">
 @import './portfolio.scss';
