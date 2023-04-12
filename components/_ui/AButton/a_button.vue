fdab9c97838efead1c9d765f68e51fa7c37def47
MIUIvanov <MIUIvanov@synergy.ru>
2023-04-12 17:31:19 +0300

    Настройка переключения версий сайта

--- components/_ui/AButton/a_button.vue
+++ components/_ui/AButton/a_button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="button" :class="classes" :disabled="disabled" @click.prevent.stop="onClickBtn">
+  <button type="button" :class="classes" :disabled="disabled" @click.prevent.stop="handleClick">
     <i :class="icons"></i>
     <slot></slot>
     <span>{{ label }}</span>
@@ -23,86 +23,89 @@ const props = defineProps({
     type: String,
     validator: (value) => ['rounded', 'square'].includes(value),
   },
+  disabled: {
+    type: Boolean,
+    required: false,
+  },
+  size: {
+    type: String,
+    validator: (value) => ['xlarge', 'large', 'medium', 'small'].includes(value),
+  },
+  type: {
+    type: String,
+    default: 'default',
+    validator: (value) => ['accent', 'primary', 'default'].includes(value),
+  },
+  fontSize: {
+    type: String,
+    validator: (value) => ['l', 'm', 's'].includes(value),
+  },
+
+  addIcon: {
+    type: String,
+    validator: (value) => ['fonts-icon'].includes(value),
+  },
+  iconPosition: {
+    type: String,
+    validator: (value) => ['left', 'right'].includes(value),
+  },
+  iconType: {
+    type: String,
+  },
+});
+
+/*
+emit
+ */
+
+const emit = defineEmits(['handleClick']);
+const handleClick = () => {
+  console.log('handleClick');
+  emit('handleClick');
+};
+
+/*
+computed
+ */
+
+const classes = computed(() => {
+  return {
+    'a-button': true,
+    [`a-button--${props.size || 'medium'}`]: true,
+    [`a-button--${props.bgColor}`]: true,
+    [`a-button--${props.addIcon}`]: true,
+    [`a-button--${props.onlyIcon}`]: true,
+    [`a-button--${props.type}`]: true,
+  };
+});
+
+const fontsize = computed(() => {
+  return {
+    [`a-font__btn-${props.fontSize}`]: true,
+  };
+});
+
+const icons = computed(() => {
+  return {
+    'a-button__icon': true,
+    [`a-button__icon--${props.addIcon}`]: true,
+    [`a-button__icon--${props.iconPosition}`]: true,
+    [`${props.iconType}`]: true,
+  };
 });
 </script>
 
-<script>
-export default {
-  name: 'AButton',
+<!--<script>-->
+<!--export default {-->
+<!--  name: 'AButton',-->
 
-  props: {
-    // label: {
-    //   type: String,
-    // },
-
-    disabled: {
-      type: Boolean,
-      required: false,
-    },
-    size: {
-      type: String,
-      validator: (value) => ['xlarge', 'large', 'medium', 'small'].includes(value),
-    },
-    type: {
-      type: String,
-      default: 'default',
-      validator: (value) => ['accent', 'primary', 'default'].includes(value),
-    },
-    fontSize: {
-      type: String,
-      validator: (value) => ['l', 'm', 's'].includes(value),
-    },
-
-    addIcon: {
-      type: String,
-      validator: (value) => ['fonts-icon'].includes(value),
-    },
-    iconPosition: {
-      type: String,
-      validator: (value) => ['left', 'right'].includes(value),
-    },
-    onlyIcon: {
-      type: String,
-      validator: (value) => ['rounded', 'square'].includes(value),
-    },
-    iconType: {
-      type: String,
-    },
-  },
-
-  computed: {
-    classes() {
-      return {
-        'a-button': true,
-        [`a-button--${this.size || 'medium'}`]: true,
-        [`a-button--${this.bgColor}`]: true,
-        [`a-button--${this.addIcon}`]: true,
-        [`a-button--${this.onlyIcon}`]: true,
-        [`a-button--${this.type}`]: true,
-      };
-    },
-    fontsize() {
-      return {
-        [`a-font__btn-${this.fontSize}`]: true,
-      };
-    },
-    icons() {
-      return {
-        'a-button__icon': true,
-        [`a-button__icon--${this.addIcon}`]: true,
-        [`a-button__icon--${this.iconPosition}`]: true,
-        [`${this.iconType}`]: true,
-      };
-    },
-  },
-
-  methods: {
-    onClickBtn() {
-      this.$emit('click');
-    },
-  },
-};
-</script>
+<!--  methods: {-->
+<!--    onClickBtn() {-->
+<!--      this.$emit('click');-->
+<!--    },-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
 
 <style scoped lang="scss">
 @import './a_button.scss';
