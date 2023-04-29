e2a00e2499fcc082b8f68bacb3f0fa1e9ada8bcb
MIUIvanov <MIUIvanov@synergy.ru>
2023-04-29 12:53:19 +0300

    Commitzen

--- composables/tools/toolsData.ts
+++ composables/tools/toolsData.ts
@@ -32,6 +32,10 @@ export const toolsData = () => {
       links: [
         { text: 'Fake API', href: 'https://fakestoreapi.com/' },
         { text: 'Настройка ESLint', href: 'https://webinmind.ru/nuxtjs/moduli/nastrojka-eslint-ot-airbnb-i-nuxtjs' },
+        {
+          text: 'Commitizen and husky',
+          href: 'https://rdevelab.ru/blog/no-category/post/git-commits-lint-with-commitlint-and-husky',
+        },
       ],
       npm: [
         { text: 'prettier', href: 'https://www.npmjs.com/package/prettier' },
