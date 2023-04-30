0461ba7873d21b4929bcb9228a03ae44773efedd
MIUIvanov <MIUIvanov@synergy.ru>
2023-04-30 17:49:58 +0300

    Настройка роутинга

---
+++ components/PostsView/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="posts-view layout-custom__wrapper">
+    <h2>{{ $route.params.id }}</h2>
+    <div class="posts-view__img-wrapper"></div>
+    <NuxtLink to="/" class="main__gallery-link">
+      <span class="a-font__s">На главную</span>
+    </NuxtLink>
+  </section>
+</template>
+
+<script setup>
+const route = useRoute();
+const imgId = route.params.id;
+</script>
+
+<style lang="scss">
+@import './posts-view.scss';
+</style>
--- components/Useful/index.vue
+++ components/Useful/index.vue
@@ -4,7 +4,16 @@
     <div class="useful__block">
       <h2>Снипеты</h2>
       <ul class="useful__list">
-        <li class="useful__item"></li>
+        <li class="useful__item">
+          <NuxtLink to="/posts/id1" class="main__gallery-link">
+            <span class="a-font__s">Post 1</span>
+          </NuxtLink>
+        </li>
+        <li class="useful__item">
+          <NuxtLink to="/posts/id2" class="main__gallery-link">
+            <span class="a-font__s">Post 2</span>
+          </NuxtLink>
+        </li>
       </ul>
     </div>
   </section>
