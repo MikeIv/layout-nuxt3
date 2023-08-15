5649c63ae7c72aaa32010fea2c8b5b99d62833aa
Иванов Михаил Юрьевич <IvanovMI@nov.mogt.ru>
2023-08-15 10:59:06 +0300

    Тестрирование подключение API

--- server/api/dataBank.js
+++ server/api/dataBank.js
@@ -1,20 +1,5 @@
-export default defineEventHandler(async () => {
-  const url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party';
-  const token = '0845425a22e3e674a4ef51f59dca016bc1c775b5';
-  const query = 'сбербанк';
-
-  const options = {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'Authorization': 'Token ' + token,
-    },
-    body: JSON.stringify({ query: query }),
-  };
-
-  const { data } = await $fetch(url, options);
+export default defineEventHandler(async (url, options) => {
+  const { data } = await useFetch(url, options);
 
   return data;
 });
