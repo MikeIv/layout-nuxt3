80af4a589849304d9622141f4e472d2a246a93a8
Иванов Михаил Юрьевич <IvanovMI@nov.mogt.ru>
2023-08-18 11:55:17 +0300

    Реализация teleport на странице

--- components/Sandbox/Teleport/index.vue
+++ components/Sandbox/Teleport/index.vue
@@ -2,14 +2,28 @@
   <section class="teleport layout-useful__wrapper">
     <CommonButtonBack link="/sandbox" />
     <h1>Телепорт</h1>
+    <AButton v-if="!showModal" @click="showModal = true" bgColor="accent" label="Показать окно" size="medium" />
+    <teleport to="body">
+      <div class="teleport__modal" v-if="showModal">
+        <h2 class="teleport__modal-header a-font__h2">Заголовок</h2>
+        <p class="teleport__modal-content a-font__m">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Accusantium dolor
+          facilis natus odit quibusdam ratione ut veniam voluptas. Corporis ducimus, hic modi nulla quasi quod similique tempora. Dolores, ea, nesciunt!</p>
+        <AButton @click="showModal = false" bgColor="accent" label="Скрыть окно" size="medium" />
+      </div>
+    </teleport>
   </section>
 
 </template>
 
 <script setup lang="ts">
+import AButton from "@/components/_ui/AButton/a_button"
+
+
+const showModal = ref(false)
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 @import "teleport.scss";
 </style>
